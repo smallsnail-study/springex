89b47125f81ec7d31753b52915817e0fe203376c
yunhuiGwak <kwakyh17@naver.com>
2023-01-25 17:42:49 +0900

    추가: 데이터베이스와의 연결 test코드 추가

--- src/test/java/org/zerock/springex/sample/SampleTests.java
+++ src/test/java/org/zerock/springex/sample/SampleTests.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @Log4j2
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
@@ -16,9 +19,22 @@ public class SampleTests {
     @Autowired
     private SampleService sampleService;
 
+    @Autowired
+    private DataSource dataSource;  // root-context.xml에 선언된 HikariCP를 주입받기 위해 DataSource타입의 변수 선언 후 @Autowired로 주입
+
     @Test
     public void testService1() {
         log.info(sampleService);
         Assertions.assertNotNull(sampleService);
+    }
+
+    @Test
+    public void testConnection() throws Exception {
+//      주입 받은 DataSource를 이용해서 데이터베이스와의 연결이 가능한지 확인
+        Connection connection = dataSource.getConnection();
+        log.info(connection);
+        Assertions.assertNotNull(connection);
+
+        connection.close();
     }
 }
