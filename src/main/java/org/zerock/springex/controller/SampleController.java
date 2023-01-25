d4c49c04c485adc3609e734541bcea5832ed6dec
yunhuiGwak <kwakyh17@naver.com>
2023-01-25 22:52:07 +0900

    추가: Formatter를 이용하지 않고 java.time의 LocalDate를 파라미터로 하는 ex3(에러확인용)

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+
 @Controller     // 해당 클래스가 스프링MVC에서 컨트롤러 역할을 한다는 것을 의미, 스프링의 Bean으로 처리되기 위해서 사용
 @Log4j2
 public class SampleController {
@@ -23,10 +25,16 @@ public class SampleController {
 
     @GetMapping("/ex2")
     // @RequestParam은 기본값(defaultValud)을 지정할 수 있는 어노테이션
-    public void ex2(@RequestParam(name = "name", defaultValue = "AAA")String name,
-                    @RequestParam(name = "age", defaultValue = "20")int age) {
+    public void ex2(@RequestParam(name = "name", defaultValue = "AAA") String name,
+                    @RequestParam(name = "age", defaultValue = "20") int age) {
         log.info("ex2.........");
         log.info("name: " + name);
         log.info("age: " + age);
+    }
+
+    @GetMapping("/ex3")     // HTTP는 문자열로 데이터를 전달하기 때문에 컨트롤러는 문자열을 기준으로 특정한 클래스의 객체를 처리
+    public void ex3(LocalDate dueDate) {    // 날짜 관련 타입의 파라미터 수집은 에러발생
+        log.info("ex3..........");
+        log.info("dueDate: " + dueDate);
     }
 }
