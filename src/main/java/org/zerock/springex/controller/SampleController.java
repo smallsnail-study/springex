546d07a6a561d6cbb4619015be498e65484576cd
yunhuiGwak <kwakyh17@naver.com>
2023-01-25 21:58:49 +0900

    생성: 스프링 MVC 컨트롤러 생성

---
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -0,0 +1,15 @@
+package org.zerock.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller     // 해당 클래스가 스프링MVC에서 컨트롤러 역할을 한다는 것을 의미, 스프링의 Bean으로 처리되기 위해서 사용
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/hello")
+    public void hello() {
+        log.info("hello..........");
+    }
+}
