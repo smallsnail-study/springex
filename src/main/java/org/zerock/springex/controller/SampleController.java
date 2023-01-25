5c58e5b1db839ae7e35501589aa3b12a6b12abec
yunhuiGwak <kwakyh17@naver.com>
2023-01-25 22:33:30 +0900

    추가: 단순 파라미터의 자동 수집 @RequestParam(기본값 설정가능) ex2 추가

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -3,6 +3,7 @@ package org.zerock.springex.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller     // 해당 클래스가 스프링MVC에서 컨트롤러 역할을 한다는 것을 의미, 스프링의 Bean으로 처리되기 위해서 사용
 @Log4j2
@@ -16,6 +17,15 @@ public class SampleController {
     @GetMapping("/ex1")
     public void ex1(String name, int age) {
         log.info("ex1............");
+        log.info("name: " + name);
+        log.info("age: " + age);
+    }
+
+    @GetMapping("/ex2")
+    // @RequestParam은 기본값(defaultValud)을 지정할 수 있는 어노테이션
+    public void ex2(@RequestParam(name = "name", defaultValue = "AAA")String name,
+                    @RequestParam(name = "age", defaultValue = "20")int age) {
+        log.info("ex2.........");
         log.info("name: " + name);
         log.info("age: " + age);
     }
