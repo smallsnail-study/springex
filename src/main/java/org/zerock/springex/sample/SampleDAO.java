3444ec8d78e5189fd7c9baeaa8e419676cc3267b
yunhuiGwak <kwakyh17@naver.com>
2023-01-25 15:31:18 +0900

    추가: 스프링 프레임워크의 어노테이션 추가

--- src/main/java/org/zerock/springex/sample/SampleDAO.java
+++ src/main/java/org/zerock/springex/sample/SampleDAO.java
@@ -1,4 +1,7 @@
 package org.zerock.springex.sample;
 
+import org.springframework.stereotype.Repository;
+
+@Repository     // 해당 클래스의 객체가 스프링에서 Bean으로 관리될 수 있도록 하는 어노테이션
 public class SampleDAO {
 }
--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -2,7 +2,9 @@ package org.zerock.springex.sample;
 
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service    // 서비스 계층의 객체를 위한 어노테이션
 @ToString
 public class SampleService {
     @Autowired
