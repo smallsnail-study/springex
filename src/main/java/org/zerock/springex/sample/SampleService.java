516cf68023903ecc6498b0cca0cb220560146571
yunhuiGwak <kwakyh17@naver.com>
2023-01-25 15:10:04 +0900

    수정: SampleService 객체 안에 SampleDAO 객체를 주입

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,4 +1,10 @@
 package org.zerock.springex.sample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
 public class SampleService {
+    @Autowired
+    private SampleDAO sampleDAO;
 }
